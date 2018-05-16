561afbd6b7e1ad7e55273d0ba2c20fa588f86d49
zhangjiaying <zhangjiaying@jiudaotech.com>
2018-05-16 12:28:31 +0800

    课程交互nav修改

--- pages/join/join.js
+++ pages/join/join.js
@@ -1,39 +1,65 @@
  var app = getApp()
  Page({
-//   data: {
-//     navLeftItems: [],
-//     navRightItems: [],
-//     curNav: 1,
-//     curIndex: 0
-//   },
-//   onLoad: function () {
+   data: {
+     listData: [
+       { "code": "01", "text": "text1", "type": "type1" },
+       { "code": "02", "text": "text2", "type": "type2" },
+       { "code": "03", "text": "text3", "type": "type3" },
+       { "code": "04", "text": "text4", "type": "type4" },
+       { "code": "05", "text": "text5", "type": "type5" },
+       { "code": "06", "text": "text6", "type": "type6" },
+       { "code": "07", "text": "text7", "type": "type7" }
+     ],
+     focus: true,
+     winWidth: 0,
+     winHeight: 0,
+     // tab切换  
+     currentTab: 0,
+   },
+    bindButtonTap: function () {
+      this.setData({
+      focus: true
+     })
+   },
+   onLoad: function () {
+     var that = this;
 
-//     var that = this
+     /** 
+      * 获取系统信息 
+      */
+     wx.getSystemInfo({
 
-//     wx.request({
-//       url: 'http://huanqiuxiaozhen.com/wemall/goodstype/typebrandList',
-//       method: 'GET',
-//       data: {},
-//       header: {
-//         'Accept': 'application/json'
-//       },
-//       success: function (res) {
-//         that.setData({
-//           navLeftItems: res.data,
-//           navRightItems: res.data
-//         })
-//       }
-//     })
-//   },
+       success: function (res) {
+         that.setData({
+           winWidth: res.windowWidth,
+           winHeight: res.windowHeight
+         });
+       }
 
-//   //事件处理函数
-//   switchRightTab: function (e) {
-//     let id = e.target.dataset.id,
-//       index = parseInt(e.target.dataset.index);
-//     this.setData({
-//       curNav: id,
-//       curIndex: index
-//     })
-//   }
+     });
+   },
+   /** 
+      * 滑动切换tab 
+      */
+   bindChange: function (e) {
 
+     var that = this;
+     that.setData({ currentTab: e.detail.current });
+
+   },
+   /** 
+    * 点击tab切换 
+    */
+   swichNav: function (e) {
+
+     var that = this;
+
+     if (this.data.currentTab === e.target.dataset.current) {
+       return false;
+     } else {
+       that.setData({
+         currentTab: e.target.dataset.current
+       })
+     }
+   }  
  })
